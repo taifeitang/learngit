7ac225ed0a4d282d0e16426ebd70e8c383073379
taifeitang <1026919284@qq.com>
2019-06-24 16:33:07 +0800

    gaoxiao

--- Pet.java
+++ Pet.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
 public class Pet{
 public static void main(String args[]){
        System.out.println("hello");
-
+		int a[]=new int[]{6,4,7,9,2,3};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(a));
   }
 } 
